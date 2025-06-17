6359fb74c194d5d7d8af3ef9cac1fa01cf725200
Sy1stm <Sy1stm@DESKTOP-M8SRUPR>
2025-06-17 10:53:13 +0900

    java.1.30

--- HelloWorld_MoriHikaru/src/curriculum_1_30_self_introduction/Person.java
+++ HelloWorld_MoriHikaru/src/curriculum_1_30_self_introduction/Person.java
@@ -7,6 +7,8 @@ public class Person {
 	private double height;
 	private double weight;
 
+	static int count = 0;
+
 	// コンストラクタを定義しインスタンスフィールドに値をセット
 	Person(String name, int age, double height, double weight) {
 		this.name = name;
@@ -14,17 +16,26 @@ public class Person {
 		this.height = height;
 		this.weight = weight; // 問題4
 
+		count++;//人数カウント
 	}
 
 	//BMIを計算して返すメソッド
 	public double bmi() {
-		return weight / (height * height);
+		return 20.0;
 	}
 
 	//情報を表示するメソッド
 	public void print() {
+		System.out.println(this.name);
+		System.out.println(this.age);
+		System.out.println(this.height);
+		System.out.println();
+
 		System.out.println("名前は" + this.name + "です");
-		System.out.println("年は" + this.age + "です");
-		System.out.printf("合計%.2f です\n ", this.bmi()); //問題4
+		System.out.println("年は" + this.age + "才です");
+		System.out.println("BMIは" + String.format("%.1f", this.bmi()) + "です"); //問題4
+		System.out.println();
+
+		System.out.println("合計" + count + "人です");
 	}
 }
